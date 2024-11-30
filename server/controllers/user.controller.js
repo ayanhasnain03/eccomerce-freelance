9d89420f7ef4da77313e1ba4e0e2fc9f0e644da2
Ayan <ayan2572006@gmail.com>
2024-11-30 20:22:27 +0530

    validation done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,9 @@
 import { asyncHandler } from "../middlewares/error.js";
+import { SendError } from "../utils/sendError.js";
 
 export const userRegister = asyncHandler(async (req, res, next) => {
-  const { name, email, password } = req.body;
-  console.log(name, email, password);
+  const { name, email, password, gender } = req.body;
+  console.log(name, email, password, gender);
   res.status(200).json({
     success: true,
     message: "User Register",
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,17 @@
+// routes/userRoutes.js
 import express from "express";
 import { userRegister } from "../controllers/user.controller.js";
 import { avtarUpload } from "../middlewares/multer.js";
+import { registerValidation, validateHandler } from "../lib/validator.js";
 
 const router = express.Router();
 
-router.post("/register", avtarUpload, userRegister);
+router.post(
+  "/register",
+  avtarUpload,
+  registerValidation,
+  validateHandler,
+  userRegister
+);
 
 export default router;
